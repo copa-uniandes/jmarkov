1aa3f2e9ee47be680aee07625e198091081cdcc8
mgabyurango <m.urango@uniandes.edu.co>
2024-03-09 18:18:04 -0500

    changes on methods for MDPs checks

--- jmarkov/dtmdp.py
+++ jmarkov/dtmdp.py
@@ -34,77 +34,20 @@ class dtmdp():
         self.transition_matrix = transition_matrix
 
     def _check_transition_matrix(self,M:np.ndarray):
-        #Check if a given transition matrix has the condition that for every row the sum of all elements is equal to 1
-        vector = np.isclose(np.sum(M, axis = 1),1,1e-5) == True  
-        #Check if a given transition matrix has the condition that every element of the matrix is between 0 and 1
-        if vector.all() and np.all((M >= 0) & (M <= 1)):
-            return True
-        else:
-            return False
-
-    def steady_state(self):
-        # computes the steady state distribution
-        if self.transition_matrix.any():
-            # sets A to be I-P and replace first column with ones for the normalizing equation
-            A = self.transition_matrix - np.eye(self.n_states, dtype=float)
-            A[:, 0] =  1
-            # sets b as the right hand vector with a 1 for the normalizing equation
-            b = np.zeros(self.n_states, dtype=float)
-            b[0] = 1
-            # solves transposed system as pi is a row vector
-            res = linalg.solve(A,b,transposed=True)
-            return res
-        else:    
-            print("Empty transition matrix")
-        return 0
-    def transient_probabilities(self,n,alpha):
-        #First, lets verify n is integer
-        if not isinstance(n,int):
-            raise ValueError("The number of transitions n must be integer")
-        #Now lets verify the demensions of the vector alpha:
-        try:
-            shape=alpha.shape
-        except:
-            print("alpha vector must be a numpy array")
-            return None
-        if shape[0]!=self.n_states:
-            raise ValueError("The dimensions of alpha vector are incorrect. It must be a vector 1xn_states")
-        
-        #Computes transient_matrix**n
-        matrix_n=np.linalg.matrix_power(self.transition_matrix,n)
-        vector=alpha@matrix_n
-        return vector
-
-
-    def first_passage_time(self, target:str):
-        #The transition matrix and the number of states are brought
-        n=self.n_states 
-        M=self.transition_matrix.copy()
-        # The identity matrix is created with size n-1 states
-        i=np.identity(n-1)
-        # Matrix of ones with n-1 rows is created
-        u=np.full([n-1,1],1)
-        # The column and row corresponding to the target state are eliminated from the transition matrix
-        p=np.delete(M,target, axis=0)
-        p=np.delete(p,target, axis=1)
-        
-        t=np.matmul(np.linalg.inv(i-p),u)
-        return t 
-
-    def occupation_time(self, n:int):
-        #computes the expected occupation time matrix in nsteps steps:
-        ocupation=np.eye(self.transition_matrix.shape[0],self.transition_matrix.shape[1])#Create an identity matrix with the same shape of transition matrix
-        for i in range(1,n+1):
-            ocupation+=np.linalg.matrix_power(self.transition_matrix,i)
-        return ocupation
-        
-       
-
-    def is_ergodic(self):
-        #TODO determines id the chain is ergodic or not
+        #TODO determines if the transition matrices are logical
         print("TODO")
         return True
     
-
+    def _check_immediate_returns(self,M:np.array):
+        #TODO determines if the immeadiate return matrix has correct dimensions
+        print("TODO")
+        return True
+    
+    def _check_discount_factor(self,beta:int):
+        #TODO determines if the discount factor is a number between 0 and 1 
+        print("TODO")
+        return True
+    
+    
 
 
